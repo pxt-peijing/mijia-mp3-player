84163f7034d81381055278fa7fb826a8844fb208
peijing <peijing@forsuc.com>
2019-07-08 10:47:34 +0800

    add description

--- namespace.ts
+++ namespace.ts
@@ -1,6 +1,6 @@
 /**
- * MakerBit
+ * 米加智造
  */
-//% color=#0fbc11 icon="\u272a" block="MakerBit"
-//% category="MakerBit"
+//% color=#0fbc11 icon="\u272a" block="米加智造"
+//% category="米加智造"
 namespace makerbit {}
